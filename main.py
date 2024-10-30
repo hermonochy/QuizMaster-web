19adf5122049ebe411f0e89dbc7e10df0ae24dc1
fcrt <you@example.com>
2024-10-30 21:48:19 +0000

    Add CORS middleware. Add basic website loading quiz.

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from glob import glob
 from fastapi import FastAPI
 import random
 import aiofiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
